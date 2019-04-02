834a8cbcd492dd0bb8999de5c8d6c1f7ff5dc67f
henry <henry.ferguson.gow@gmail.com>
2019-04-02 11:27:04 +0100

    tidying

--- spdist_sim.R
+++ spdist_sim.R
@@ -44,14 +44,32 @@ make_bc_mask <- function(occurence, env_data) {
   return(ret)
 }
 
-sdm_both <- function(occurence, mask, env, model = "glm") {
-  # split occurence into training and test
+make_dat <- function(occurence, bg, env) {
   oc_kf <- kfold(occurence, 5)
   oc_train <- occurence[oc_kf != 1, ]
   oc_test <- occurence[oc_kf == 1, ]
+  bg_kf <- dismo::kfold(bg, 5)
+  bg_train <- bg[bg_kf != 1, ]
+  bg_test <- bg[bg_kf == 1, ]
+  train <- rbind(oc_train, bg_train)
+  pa <- c(
+    rep(1, nrow(oc_train)),
+    rep(0, nrow(bg_train))
+  )
+  traindat <- raster::extract(env, train)
+  traindat <- data.frame(cbind(pa = pa, traindat))
+  test_oc <- data.frame(raster::extract(env, oc_test))
+  test_bg <- data.frame(raster::extract(env, bg_test))
 
-  # Prepare MASKED data
-    # pseudoabsences from outside mask
+  ret <- list(
+    traindat = traindat,
+    test_pres = test_oc,
+    test_bg = test_bg)
+  return(ret)
+}
+
+sdm_both <- function(occurence, mask, env, model = "glm") {
+  # Prepare MASKED absences
     mask[mask == 1] <- NA
     if (nrow(occurence) >= 50) {
       bg_m <- dismo::randomPoints(mask, nrow(occurence))
@@ -59,57 +77,27 @@ sdm_both <- function(occurence, mask, env, model = "glm") {
       bg_m <- dismo::randomPoints(mask, 50)
     }
 
-    # make data object
-    bg_m_kf <- dismo::kfold(bg_m, 5)
-    bg_m_train <- bg_m[bg_m_kf != 1, ]
-    bg_m_test <- bg_m[bg_m_kf == 1, ]
-
-    train_m <- rbind(oc_train, bg_m_train)
-    pa <- c(
-      rep(1, nrow(oc_train)),
-      rep(0, nrow(bg_m_train))
-    )
-    traindat_m <- raster::extract(env, train_m)
-    traindat_m <- data.frame(cbind(pa = pa, traindat_m))
-
-    test_oc <- data.frame(raster::extract(env, oc_test))
-    test_bg_m <- data.frame(raster::extract(env, bg_m_test))
-
-  # prepare UNMASKED data
-    # standard background pseudoabsences
+  # prepare UNMASKED absences
     if (nrow(occurence) >= 50) {
       bg_u <- dismo::randomPoints(env, nrow(occurence))
     } else {
       bg_u <- dismo::randomPoints(env, 50)
     }
 
-    bg_u_kf <- dismo::kfold(bg_u, 5)
-    bg_u_train <- bg_u[bg_u_kf != 1, ]
-    bg_u_test <- bg_u[bg_u_kf == 1, ]
-
-    train_u <- rbind(oc_train, bg_u_train)
-    pa <- c(
-      rep(1, nrow(oc_train)),
-      rep(0, nrow(bg_u_train))
-    )
-    traindat_u <- raster::extract(env, train_u)
-    traindat_u <- data.frame(cbind(pa = pa, traindat_u))
-
-    test_bg_u <- data.frame(raster::extract(env, bg_u_test))
-
-  # fit both models.
   m <- stats::formula(paste("pa ~", paste(colnames(traindat_m)[-1], 
     collapse = "+")))
+  dat_m <- make_dat(occurence, bg_m, env)
+  dat_u <- make_dat(occurence, bg_u, env)
 
   if (model == "glm") {
-    mod_m <- glm(formula(m), data = traindat_m, 
+    mod_m <- glm(formula(m), data = dat_m, 
       family = binomial(link = "logit"))
-    mod_u <- glm(formula(m), data = traindat_u, 
+    mod_u <- glm(formula(m), data = dat_u, 
       family = binomial(link = "logit"))
   } else if (model == "rf") {
-    mod_m <- suppressWarnings(randomForest::randomForest(m, data = traindat_m,
+    mod_m <- suppressWarnings(randomForest::randomForest(m, data = dat_m,
       na.action = na.omit))
-    mod_u <- suppressWarnings(randomForest::randomForest(m, data = traindat_u,
+    mod_u <- suppressWarnings(randomForest::randomForest(m, data = dat_u,
       na.action = na.omit))
   }
 
@@ -121,11 +109,13 @@ sdm_both <- function(occurence, mask, env, model = "glm") {
   pred_m <- pred_m > threshold(eval_m, "spec_sens")
   pred_u <- pred_u > threshold(eval_u, "spec_sens")
 
+  comparison <- c(masked_auc = eval_m@auc, unmasked_auc = eval_u@auc)
+
   ret <- list(
+    comparison = comparison,
     masked = list(model = mod_m, evaluation = eval_m, prediction = pred_m),
     unmasked = list(model = mod_u, evaulation = eval_u, prediction = pred_u)
   )
-
   return(ret)
 }
 
@@ -190,4 +180,13 @@ points(vnil_test, pch = 16, cex = 0.8, col = "red")
 # compare masked to unmasked performance
 comp_glm <- sdm_both(vnil_test, bc_mask$mask, env, model = "glm")
 comp_rf <- sdm_both(vnil_test, bc_mask$mask, env, model = "rf")
+
+# plot predictions...
+par(mfrow = c(1, 2))
+plot(comp_glm$masked$prediction)
+plot(comp_glm$unmasked$prediction)
+
+par(mfrow = c(1, 2))
+plot(comp_rf$masked$prediction)
+plot(comp_rf$unmasked$prediction)
 
