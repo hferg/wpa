72b0b23e4905ef14cf90a872cbc7dde11239edc8
henry <henry.ferguson.gow@gmail.com>
2019-04-02 14:04:47 +0100

    same

--- spdist_sim.R
+++ spdist_sim.R
@@ -38,12 +38,12 @@ make_bc_mask <- function(occurence, env_data) {
 
   # predict and mask
   x <- dismo::predict(env, bc)
-  x <- x > bc_t
-  ret <- list(model = bc, evaluation = bc_ev, mask = x)
+  xx <- x > bc_t
+  ret <- list(model = bc, evaluation = bc_ev, surface = x, mask = xx)
   return(ret)
 }
 
-make_dat <- function(occurence, bg, env) {
+make_dat_int <- function(occurence, bg, env) {
   oc_kf <- dismo::kfold(occurence, 5)
   oc_train <- occurence[oc_kf != 1, ]
   oc_test <- occurence[oc_kf == 1, ]
@@ -67,24 +67,67 @@ make_dat <- function(occurence, bg, env) {
   return(ret)
 }
 
-sdm_both <- function(occurence, mask, env, model = "glm", full_kfold = FALSE) {
+make_dat_ext <- function(occurence_train, occurence_test, bg, env) {
+  oc_train <- occurence_train
+  oc_test <- occurence_test
+  bg_kf <- dismo::kfold(bg, 5)
+  bg_train <- bg[bg_kf != 1, ]
+  bg_test <- bg[bg_kf == 1, ]
+  train <- rbind(oc_train, bg_train)
+  pa <- c(
+    rep(1, nrow(oc_train)),
+    rep(0, nrow(bg_train))
+  )
+  traindat <- raster::extract(env, train)
+  traindat <- data.frame(cbind(pa = pa, traindat))
+  test_oc <- data.frame(raster::extract(env, oc_test))
+  test_bg <- data.frame(raster::extract(env, bg_test))
+
+  ret <- list(
+    traindat = traindat,
+    test_pres = test_oc,
+    test_bg = test_bg)
+  return(ret)
+}
+
+sdm_both <- function(occurence_train, occurence_test, mode = "mask", env, 
+  model = "glm", full_kfold = FALSE, eval = "external") {
   # Prepare MASKED absences
-    mask[mask == 1] <- NA
-    if (nrow(occurence) >= 50) {
-      bg_m <- dismo::randomPoints(mask, nrow(occurence))
-    } else {
-      bg_m <- dismo::randomPoints(mask, 50)
+    bc_mod <- make_bc_mask(occurence_train, env)
+    
+    if (mode == "mask") {
+      bc_mod$mask[bc_mod$mask == 1] <- NA      
+      if (nrow(occurence_train) >= 50) {
+        bg_m <- dismo::randomPoints(bc_mod$mask, nrow(occurence_train))
+      } else {
+        bg_m <- dismo::randomPoints(bc_mod$mask, 50)
+      }
+    } else if (mode == "weighted") {
+      # sample points based on the bioclim probability surface
+      if (nrow(occurence_train) >= 50) {
+        bg_m <- dismo::randomPoints(bc_mod$surface, nrow(occurence_train), 
+          prob = TRUE)
+      } else {
+        bg_m <- dismo::randomPoints(bc_mod$surface, 50, prob = TRUE)
+      }
     }
 
   # prepare UNMASKED absences
-    if (nrow(occurence) >= 50) {
-      bg_u <- dismo::randomPoints(env, nrow(occurence))
+    if (nrow(occurence_train) >= 50) {
+      bg_u <- dismo::randomPoints(env, nrow(occurence_train))
     } else {
       bg_u <- dismo::randomPoints(env, 50)
     }
 
-  dat_m <- make_dat(occurence, bg_m, env)
-  dat_u <- make_dat(occurence, bg_u, env)
+  if (eval == "internal") {
+    dat_m <- make_dat_int(occurence_train, bg_m, env)
+    dat_u <- make_dat_int(occurence_train, bg_u, env)
+  } else if (eval == "external") {
+    dat_m <- make_dat_ext(occurence_train, occurence_test, bg_m, env)
+    dat_u <- make_dat_ext(occurence_train, occurence_test, bg_u, env)
+  }
+
+  
   m <- stats::formula(paste("pa ~", paste(colnames(dat_m$traindat)[-1], 
     collapse = "+")))
 
@@ -164,21 +207,64 @@ vnil_train <- rbind(vnil_s, vnil_w)
 
 vnil_e <- vnil[vnil$x > 2, ]
 vnil_e <- vnil_e[vnil_e$y > -20, ]
-vnil_test <- vnil_e
-
+vnil_test <- vnil_
+e
 raster::plot(env[[1]])
 points(vnil_train, pch = 16, cex = 0.8, col = "black")
 points(vnil_test, pch = 16, cex = 0.8, col = "red")
-
-bc_mask <- make_bc_mask(vnil_train, env)
 
 raster::plot(bc_mask$mask)
 points(vnil_train, pch = 16, cex = 0.8, col = "black")
 points(vnil_test, pch = 16, cex = 0.8, col = "red")
 
 # compare masked to unmasked performance
-comp_glm <- sdm_both(vnil_test, bc_mask$mask, env, model = "glm")
-comp_rf <- sdm_both(vnil_test, bc_mask$mask, env, model = "rf")
+comp_glm_m <- sdm_both(
+  occurence_train = vnil_train, 
+  occurence_test = vnil_test, 
+  mode = "mask", 
+  env = env, 
+  model = "glm", 
+  eval = "external")
+comp_glm_w <- sdm_both(
+  occurence_train = vnil_train, 
+  occurence_test = vnil_test, 
+  mode = "weighted", 
+  env = env, 
+  model = "glm", 
+  eval = "external")
+comp_rf_m <- sdm_both(
+  occurence_train = vnil_train, 
+  occurence_test = vnil_test, 
+  mode = "mask", 
+  env = env, 
+  model = "rf", 
+  eval = "external")
+comp_rf_w <- sdm_both(
+  occurence_train = vnil_train, 
+  occurence_test = vnil_test, 
+  mode = "weighted", 
+  env = env, 
+  model = "rf", 
+  eval = "external")
+
+# NOTES - this seems to improve GLM fit consistently, but not random forest.
+# weighted mode seems to have very little effect.
+
+# replicate the GLM analysis to check...
+cl <- parallel::makeForkCluster(10)
+res <- parallel::parLapply(cl, 1:50, function(x) {
+  comp_glm_m <- sdm_both(
+  occurence_train = vnil_train, 
+  occurence_test = vnil_test, 
+  mode = "mask", 
+  env = env, 
+  model = "glm", 
+  eval = "external")
+})
+parallel::stopCluster(cl)
+
+comps <- data.frame(do.call(rbind, lapply(res, function(x) {x$comparison})))
+comps$diff <- comps$masked_auc - comps$unmasked_auc
 
 # plot predictions...
 par(mfrow = c(1, 2))
